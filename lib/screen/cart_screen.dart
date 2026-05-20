e0c2471201b2762e6efa37ef4910bf12762ee823
Chhai Chivon <chivon.chhai.kh@gmail.com>
2026-05-20 20:26:51 +0700

    Update Home Screen

--- lib/screen/cart_screen.dart
+++ lib/screen/cart_screen.dart
@@ -89,8 +89,7 @@ class _CartScreenState extends State<CartScreen> {
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Text(
-                              "${m.phoneNumber}",
+                            Text("${m.phoneNumber}",
                               style: TextStyle(fontSize: 18),
                             ),
                           ],
